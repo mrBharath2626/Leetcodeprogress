54d362f71f8711fd4451733c6aff0979c6bc0ab5
mrBharath2626 <bharathsprivatemail@gmail.com>
2026-07-14 13:58:20 +0530

    Create 4115-MinimumDistanceBetweenThreeEqualElementsI

---
+++ 4115-MinimumDistanceBetweenThreeEqualElementsI/4115-MinimumDistanceBetweenThreeEqualElementsI.py
@@ -0,0 +1,11 @@
+# Last updated: 7/14/2026, 1:58:19 PM
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        n, M=len(nums), max(nums)
+        pos=[(-1, -1) for _ in range(M+1)]
+        ans=1<<32
+        for k, x in enumerate(nums):
+            if pos[x][1]!=-1:
+                ans=min(ans, (k-pos[x][1])<<1)
+            pos[x]=k, pos[x][0]
+        return -1 if ans==1<<32 else ans
