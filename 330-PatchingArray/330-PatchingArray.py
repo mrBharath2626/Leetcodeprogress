bf1d2143e63aefba9e2dc9edafb96e8ac7c3a0fc
mrBharath2626 <bharathsprivatemail@gmail.com>
2026-07-14 13:59:05 +0530

    Create 330-PatchingArray

---
+++ 330-PatchingArray/330-PatchingArray.py
@@ -0,0 +1,16 @@
+# Last updated: 7/14/2026, 1:59:05 PM
+class Solution:
+    def minPatches(self, nums, n):
+        miss = 1
+        patches = 0
+        i = 0
+
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patches += 1
+
+        return patches
