26eae5389357f3693b5e517fb7b3d5dd1b8c82aa
mrBharath2626 <bharathsprivatemail@gmail.com>
2026-07-14 13:58:46 +0530

    Create 2199-TwoFurthestHousesWithDifferentColors

---
+++ 2199-TwoFurthestHousesWithDifferentColors/2199-TwoFurthestHousesWithDifferentColors.py
@@ -0,0 +1,18 @@
+# Last updated: 7/14/2026, 1:58:45 PM
+class Solution:
+    def maxDistance(self, A: List[int]) -> int:
+        n = len(A)
+        left, right = 0, n - 1
+
+        for i in range(n):
+            if A[i] ^ A[-1]:
+                left = i
+                break
+
+        for i in range(n - 1, -1, -1):
+            if A[i] ^ A[0]:
+                right = i
+                break
+
+        return max(n - 1 - left, right)
+        
